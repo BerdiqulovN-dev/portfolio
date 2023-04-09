668db2c301ca6e556ef01785cce738efc7a5879b
BerdiqulovN-dev <navruzbekturalov0333@gmail.com>
2023-04-09 07:51:00 +0500

    0.0.2

--- Month-5/lesson-1/src/App.jsx
+++ Month-5/lesson-1/src/App.jsx
@@ -2,14 +2,33 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Intro from "./components/Intro/Intro";
 import Main from "./components/Main/Main";
-export let img= "./assets/Intro-img.png";
-// console.log(img);
+import img from "./assets/images/twitter.svg";
+
 const App = () => {
 	return (
 		<>
-			<Header />
-			<Intro />
-			<Main />
+			<main>
+				<div className="p-5 shadow m-5 rounded mx-auto">
+					<form action="#" className="form">
+						<label htmlFor="title" className="w-50 d-block mx-auto ">
+							<p className="text-primary fw-bold text-uppercase">Enter title</p>
+							<input type="text"
+							id="title"
+							className="form-control p-3 w-100" />
+						</label>
+						<label htmlFor="title" className="w-50 d-block mx-auto ">
+							<p className="text-primary fw-bold text-uppercase">Enter title</p>
+							<input type="text"
+							id="title"
+							className="form-control p-3 w-100" />
+						</label>
+					</form>
+				</div>
+			</main>
+
+			{/* <Header /> */}
+			{/* <Intro /> */}
+			{/* <Main /> */}
 		</>
 	);
 };
--- Month-5/lesson-1/src/components/Intro/Intro.jsx
+++ Month-5/lesson-1/src/components/Intro/Intro.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import "./index.scss";
-import  {img}  from "../../App";
-// console.log(img);
+// import  {img}  from "../../App";
+// import imgIntro from "../../assets/images/Intro-img";
+
 const Intro = () => {
 	return (
-		<section className="Intro">
-			<div className="container">
-				<div className="Intro__inner">
-					<div className="Intro__left">
-						<h1 className="Intro__left-title">
-							Hi, I am John, Creative Technologist
-						</h1>
-						<p className="Intro__left-text">
-							Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-							amet sint. Velit officia consequat duis enim velit mollit.
-							Exercitation veniam consequat sunt nostrud amet.
-						</p>
-						<button className="Intro__left-btn">Download Resume</button>
-					</div>
-					<div className="Intro__right">
-                        <img src={img} alt="" className="Intro__right-img" />
-                    </div>
-				</div>
-			</div>
+		<section className="header">
+
 		</section>
+
+		// <section className="Intro">
+		// 	<div className="container">
+		// 		<div className="Intro__inner">
+		// 			<div className="Intro__left">
+		// 				<h1 className="Intro__left-title">
+		// 					Hi, I am John, Creative Technologist
+		// 				</h1>
+		// 				<p className="Intro__left-text">
+		// 					Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+		// 					amet sint. Velit officia consequat duis enim velit mollit.
+		// 					Exercitation veniam consequat sunt nostrud amet.
+		// 				</p>
+		// 				<button className="Intro__left-btn">Download Resume</button>
+		// 			</div>
+		// 			<div className="Intro__right">
+		//                 <img src={imgIntro} alt="" className="Intro__right-img" />
+		//             </div>
+		// 		</div>
+		// 	</div>
+		// </section>
 	);
 };
 
